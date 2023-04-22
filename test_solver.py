cec32419b9ef4371cc2442b893896bdacbba69ad
Gabor Meszaros <jegkocka88@gmail.com>
2023-04-22 17:38:46 +0200

    Unit tests added.

--- test_solver.py
+++ test_solver.py
@@ -7,9 +7,26 @@ from .solver import describe_moves, solve_digits, solve_digits_with_moves
     "moves,expected",
     [
         (
-            [[1,1], [2]],
+            [[1, 1], [2]],
             ["1+1"],
-        )
+        ),
+        (
+            [[1, 1], [0]],
+            ["1-1"],
+        ),
+                (
+            [[2, 3], [6]],
+            ["3*2"],
+        ),
+                (
+            [[6, 2], [3]],
+            ["6/2"],
+        ),
+        (
+            [[1, 2, 5],[3, 5], [15]],
+            ["2+1", "5*3"],
+        ),
+
     ]
 )
 def test_moves(
@@ -23,13 +40,47 @@ def test_moves(
     "numbers,target,solution",
     [
         (
-            [1,1,],
+            [1, 1],
             2,
-            [[[1,1], [2]]],
+            [[[1, 1], [2]]],
+        ),
+        (
+            [2, 3],
+            6,
+            [[[2, 3], [6]]],
+        ),
+        (
+            [2, 3],
+            1,
+            [[[2, 3], [1]]],
+        ),
+        (
+            [3, 9],
+            3,
+            [[[3, 9], [3]]],
         )
     ]
 )
 def test_unique_single_steps(
+        numbers,
+        target,
+        solution,
+):
+    assert solve_digits(numbers, target) == solution
+
+
+
+@pytest.mark.parametrize(
+    "numbers,target,solution",
+    [
+        (
+            [1, 2, 4],
+            12,
+            [[[1, 2, 4], [3, 4], [12]]],
+        ),
+    ]
+)
+def test_unique_solution_multiple_steps(
         numbers,
         target,
         solution,
