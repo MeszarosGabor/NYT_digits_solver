db7a593e5d825a6b7fd6fe8740e8f654036b3fb5
Gabor Meszaros <jegkocka88@gmail.com>
2023-04-22 20:17:51 +0200

    Refactor: next move generation moved to generator.

--- solver.py
+++ solver.py
@@ -5,31 +5,40 @@ from collections import deque
 import click
 
 
+def generate_next_moves(
+        move: typing.List[int]) -> typing.List[int]:
+     for i in range(len(move)):
+        for j in range(i + 1, len(move)):
+            remainders = [
+                move[k] for k in range(len(move)) if k not in [i, j]
+            ]
+            operations = {
+                "+": lambda x, y: x + y,
+                "-": lambda x, y: abs(x - y),
+                "*": lambda x, y: x * y,
+            }
+            if move[j] != 0 and move[i] % move[j] == 0:
+                operations["/"] = lambda x, y: x // y
+            if move[i] != 0 and move[j] % move[i] == 0:
+                operations["/"] = lambda x, y: y // x
+            for op_sym, op in operations.items():
+                next_move_candidate = sorted(
+                    remainders + [op(move[i], move[j])])
+                op_description = (
+                    f"{max(move[i], move[j])}"
+                    f"{op_sym}"
+                    f"{min(move[i], move[j])}"
+                )
+
+                yield next_move_candidate, op_description
+
+
 def describe_moves(moves: typing.List[typing.List[int]]) -> typing.List[str]:
     descriptions = []
     for move, next_move in zip(moves, moves[1:]):
-        for i in range(len(move)):
-            for j in range(i + 1, len(move)):
-                remainders = [
-                    move[k] for k in range(len(move)) if k not in [i, j]
-                ]
-                operations = {
-                    "+": lambda x, y: x + y,
-                    "-": lambda x, y: abs(x - y),
-                    "*": lambda x, y: x * y,
-                }
-                if move[j] != 0 and move[i] % move[j] == 0:
-                    operations["/"] = lambda x, y: x // y
-                if move[i] != 0 and move[j] % move[i] == 0:
-                    operations["/"] = lambda x, y: y // x
-                for op_sym, op in operations.items():
-                    next_move_candidate = sorted(
-                        remainders + [op(move[i], move[j])])
-                    if next_move_candidate == next_move:
-                        descriptions.append(
-                            f"{max(move[i], move[j])}"
-                            f"{op_sym}"
-                            f"{min(move[i], move[j])}")
+        for next_move_candidate, op_descr in generate_next_moves(move):
+            if next_move_candidate == next_move:
+                descriptions.append(op_descr)
     return descriptions
 
 
@@ -44,33 +53,17 @@ def solve_digits(
     while todo:
         act = todo.popleft()
         tip = act[-1]
-        for i in range(len(tip)):
-            for j in range(i + 1, len(tip)):
-                remainders = [
-                    tip[k] for k in range(len(tip)) if k not in [i, j]
-                    ]
-                operations = [
-                    lambda x, y: x + y,
-                    lambda x, y: abs(x - y),
-                    lambda x, y: x * y,
-                ]
-                if tip[j] != 0 and tip[i] % tip[j] == 0:
-                    operations.append(lambda x, y: x // y)
-                if tip[i] != 0 and tip[j] % tip[i] == 0:
-                    operations.append(lambda x, y: y // x)
-
-                for op in operations:
-                    new_tip = sorted(remainders + [op(tip[i], tip[j])])
-                    if len(new_tip) > 1:
-                        new_act = act[::]
-                        new_act.append(new_tip)
-                        todo.append(new_act)
-                    elif new_tip[0] == target:
-                        new_sol_candidate = act + [new_tip]
-                        if new_sol_candidate not in solutions:
-                            solutions.append(new_sol_candidate)
-                            if how_many_sols and len(solutions) >= how_many_sols:
-                                return solutions
+        for new_tip, _ in generate_next_moves(tip):
+            if len(new_tip) > 1:
+                new_act = act[::]
+                new_act.append(new_tip)
+                todo.append(new_act)
+            elif new_tip[0] == target:
+                new_sol_candidate = act + [new_tip]
+                if new_sol_candidate not in solutions:
+                    solutions.append(new_sol_candidate)
+                    if how_many_sols and len(solutions) >= how_many_sols:
+                        return solutions
     return solutions
 
 
