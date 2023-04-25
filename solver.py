38c6369940ff0acffaede01d655b439e92268819
Gabor Meszaros <jegkocka88@gmail.com>
2023-04-25 13:44:36 +0200

    Refactor: separated runner logic from solver.

--- solver.py
+++ solver.py
@@ -2,8 +2,6 @@ import time
 import typing
 from collections import deque
 
-import click
-
 
 def generate_next_moves(
         move: typing.List[int]) -> typing.List[int]:
@@ -76,31 +74,3 @@ def solve_digits_with_moves(numbers: typing.List[int],
         for solution in solve_digits(numbers, target, how_many_sols)
     ]
 
-
-@click.command()
-@click.option(
-    "-n", "--numbers", type=str,
-    help="Comma-separated list of numbers")
-@click.option(
-    "-t", "--target", type=int,
-    help="target number to be built")
-@click.option(
-    "-c", "--how_many_solutions", type=int,
-    help="The number of solutions the solver should return (None=find all)",
-    default=None)
-def main(numbers, target, how_many_solutions):
-    numbers = [int(number) for number in numbers.split(",")]
-    t0 = time.time()
-    solutions = solve_digits_with_moves(
-        numbers, target, how_many_sols=how_many_solutions)
-    t1 = time.time()
-    print(
-        f"Found {len(solutions)} solutions. Took {round(t1 - t0, 2)} seconds.")
-    for sol in solutions:
-        for move in sol:
-            print(move)
-        print("\n")
-
-
-if __name__ == "__main__":
-    main()
